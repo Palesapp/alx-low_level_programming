925e8a90618e09e645aea56e850135ce081aa4a7
Palesapp <nnupalesa.pp@gmail.com>
2023-06-15 12:56:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-/**
- * main - Printing
- *
- * Return: Always 0 (Successful)
- */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return 0;
 }
+
+
+
+
+
+
+
+
+
 
 
 
