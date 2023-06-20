a328e774a7d3121f1d86899d1e720fb61c58e61d
Palesapp <nnupalesa.pp@gmail.com>
2023-06-20 06:07:36 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
 *Description: print_alaphabet - Prints alphabet in lowercase
 *
-*
+*Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
 	int m;
 
-	for ( m = 97; m <= 122; m++)
+	for (m = 97; m <= 122; m++)
 	{
 		_putchar(m);
 	}
