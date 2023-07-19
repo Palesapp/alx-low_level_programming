7486d278c189b6cbc0d63d27aed02b4663b96c32
Palesapp <nnupalesa.pp@gmail.com>
2023-07-19 06:07:56 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,7 +7,7 @@
  * Return: the pointer to the function corresponding to
  * the operator given as a paramter
  */
-int (*get_op_func(char *s)(int, int))
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
@@ -17,3 +17,9 @@ int (*get_op_func(char *s)(int, int))
 		{"%", op_mod},
 		{NULL, NULL},
 	};
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+	return (ops[i].f);
+}
